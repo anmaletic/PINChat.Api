f27687688789c71d28517deb60a6419264e15ba9
AnMal <junkie_@msn.com>
2024-02-20 00:14:38 +0100

    Updated GroupModel

--- PINChat.Api.Library/Models/GroupDbModel.cs
+++ PINChat.Api.Library/Models/GroupDbModel.cs
@@ -4,5 +4,6 @@ public class GroupDbModel
 {
     public string? Id { get; set; }
     public string? Name { get; set; }
+    public byte[]? Avatar { get; set; }
     public List<string> ContactIds { get; set; } = new();
 }
