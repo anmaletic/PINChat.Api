1efcb129a529d51c743decce90229420f0f6c6b4
AnMal <junkie_@msn.com>
2024-02-20 07:58:26 +0100

    Remove ImageGeneration

--- PINChat.Api.Library/DataAccess/ImageData.cs
+++ PINChat.Api.Library/DataAccess/ImageData.cs
@@ -27,46 +27,7 @@ public class ImageData : IImageData
         var p = new { Id = groupId };
         
         var groupDb = _sql.LoadData<GroupDbModel, dynamic>("[PINChat].[spGroups_GetById]", p, "PINChatData").FirstOrDefault();
-
-        var avatar = GenerateImageFromInitials($"{groupDb!.Name![0]}");
         
-        return avatar;
-    }
-
-    private byte[] GenerateImageFromInitials(string? name)
-    {
-        var info = new SKImageInfo(100, 100); // Set the size of the bitmap
-        var bitmap = new SKBitmap(info);
-    
-        using (var surface = SKSurface.Create(info, bitmap.GetPixels(), info.RowBytes))
-        {
-            var canvas = surface.Canvas;
-
-            canvas.Clear(SKColors.LightSlateGray); // Set background color
-        
-            // Create a paint object for the text
-            using (var paint = new SKPaint())
-            {
-                paint.Color = SKColors.White; // Set text color
-                paint.TextAlign = SKTextAlign.Center;
-                paint.TextSize = 36;
-                paint.FakeBoldText = true;
-
-                // Calculate the position to center the text on the bitmap
-                var x = info.Width / 2;
-                var y = (info.Height + paint.TextSize / 2) / 2;
-
-                // Draw the text on the bitmap
-                canvas.DrawText(name, x, y, paint);
-            }
-        }
-        
-        using (var image = SKImage.FromBitmap(bitmap))
-        using (var data = image.Encode(SKEncodedImageFormat.Png, 300))
-        using (var stream = data.AsStream())
-        {
-            // Get the byte array from the encoded data
-            return data.ToArray();
-        }
+        return groupDb!.Avatar!;
     }
 }
